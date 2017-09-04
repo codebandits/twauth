c0560e4bbc9fae930b036ed9bdd7cc92bf1e392e
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-09-03 21:44:51 -0700

    Add a e2e test for chat

---
+++ e2e/tests/chat.test.js
@@ -0,0 +1,15 @@
+import nightmare from 'nightmare'
+
+const CHAT_URL = process.env.CHAT_URL || 'http://localhost:3000'
+
+describe('twauth chat', function () {
+
+    test('prompts the user to login', async function () {
+        let page = nightmare().goto(CHAT_URL)
+
+        let text = await page.evaluate(() => document.body.innerText)
+            .end()
+
+        expect(text).toContain("Login")
+    })
+})
