e58a7c8cc9ae6178f0af1761e3d70d73046806df
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-09-07 21:58:00 -0700

    Add a Tweet button to the login page

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import { Share } from 'react-twitter-widgets'
+
+import uuid from 'uuid'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>Login</h1>
-      </div>
-    );
-  }
+    render() {
+        const message = uuid()
+        return (
+            <div className="App">
+              <h1>Tweet to login:</h1>
+              <Share url="''" options={{text: message, hashtags: 'twauth', size: 'large'}}/>
+            </div>
+        );
+    }
 }
 
 export default App;
