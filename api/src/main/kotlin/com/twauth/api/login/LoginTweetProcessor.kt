eb2af5b5d8553bad8502bfab09de704f984663af
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-09-10 03:11:42 -0700

    Add logging to LoginTweetProcessor

--- api/src/main/kotlin/com/twauth/api/login/LoginTweetProcessor.kt
+++ api/src/main/kotlin/com/twauth/api/login/LoginTweetProcessor.kt
@@ -3,6 +3,7 @@ package com.twauth.api.login
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.codebandits.results.Failure
 import io.github.codebandits.results.Success
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.TextMessage
@@ -19,31 +20,43 @@ class LoginTweetProcessor(
         private val hashtag: String
 ) : StatusListener {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun onTrackLimitationNotice(numberOfLimitedStatuses: Int) {
+        logger.warn("received TrackLimitationNotice: {}", numberOfLimitedStatuses)
     }
 
     override fun onStallWarning(warning: StallWarning) {
+        logger.warn("received StallWarning", warning)
     }
 
     override fun onException(ex: Exception) {
+        logger.error("received Exception", ex)
     }
 
     override fun onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {
+        logger.info("received DeletionNotice")
     }
 
     override fun onStatus(status: Status) {
+        logger.info("received Status: ", status)
+
         val message = status.text.replace("#$hashtag", "").trim()
         val user = loginClientManager.get(message)
         when (user) {
             is Success -> {
+                logger.info("""received login tweet "{}" from user {}""", user.content.message, status.user.screenName)
+
                 val authentication = LoginMessage.Payload.Authentication(token = "batman", handle = status.user.screenName)
                 val loginMessage = LoginMessage(messageType = LoginMessage.MessageType.AUTHENTICATION, payload = authentication)
                 val json = ObjectMapper().writeValueAsString(loginMessage)
                 user.content.session.sendMessage(TextMessage(json))
             }
+            is Failure -> logger.info("""received unexpected tweet "{}" from user {}""", message, status.user.screenName)
         }
     }
 
     override fun onScrubGeo(userId: Long, upToStatusId: Long) {
+        logger.info("received ScrubGeo: {} {}", userId, upToStatusId)
     }
 }
