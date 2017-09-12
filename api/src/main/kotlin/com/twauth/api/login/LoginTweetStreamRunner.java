8efee18a4b42c8f8adc3b149acb0324abfee9529
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-09-11 21:16:55 -0700

    Add more logging to API

--- api/src/main/kotlin/com/twauth/api/login/LoginTweetStreamRunner.java
+++ api/src/main/kotlin/com/twauth/api/login/LoginTweetStreamRunner.java
@@ -1,6 +1,8 @@
 package com.twauth.api.login;
 
 import com.twauth.api.tweet.HashtagTweetStreamFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,6 +11,8 @@ import twitter4j.TwitterStream;
 
 @Component
 public class LoginTweetStreamRunner {
+
+    private Logger logger = LoggerFactory.getLogger(LoginTweetStreamRunner.class);
 
     private HashtagTweetStreamFactory hashtagTweetStreamFactory;
     private String hashtag;
@@ -25,6 +29,7 @@ public class LoginTweetStreamRunner {
 
     @EventListener(ContextRefreshedEvent.class)
     public void connectToStream() {
+        logger.info("Context refreshed, creating twitter stream");
         loginTweetStream = hashtagTweetStreamFactory.create(hashtag);
     }
 }
